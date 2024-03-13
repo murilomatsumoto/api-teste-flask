26602850a34b499eb677ad9e2dcffe2dbcf3f469
murilomatsumoto <murilomatsumoto@gmail.com>
2024-03-13 16:06:11 -0400

    ajuste2

--- app/main.py
+++ app/main.py
@@ -74,4 +74,4 @@ def get_tickets():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=443, debug=True)
+    app.run(host='0.0.0.0', debug=True)
